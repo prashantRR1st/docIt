4fc8c3af4456b323c8eb4cd1a5986d3e82dae9d3
Prashant Revaneti <PR0007@dhltd.corp>
2017-11-20 14:54:27 -0500

    Native Audio Plugin Test (Added local file name to json media data)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpModule} from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Camera } from '@ionic-native/camera';
+import { NativeAudio } from '@ionic-native/native-audio';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule, Storage } from '@ionic/storage';
@@ -65,6 +66,7 @@ export function provideSettings(storage: Storage) {
     Items,
     User,
     Camera,
+    NativeAudio,
     SplashScreen,
     StatusBar,
     { provide: Settings, useFactory: provideSettings, deps: [Storage] },
