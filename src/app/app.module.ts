2ab17fbb5c1fc337bad341f9c9a88fcbbcf809d3
Prashant Revaneti <PR0007@dhltd.corp>
2017-12-06 13:27:44 -0500

    File Plugin Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Camera } from '@ionic-native/camera';
 import { NativeAudio } from '@ionic-native/native-audio';
 import { Base64 } from '@ionic-native/base64';
+import { File } from '@ionic-native/file';
 import { MediaCapture } from '@ionic-native/media-capture';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -70,6 +71,7 @@ export function provideSettings(storage: Storage) {
     Camera,
     NativeAudio,
     Base64,
+    File,
     MediaCapture,
     SplashScreen,
     StatusBar,
